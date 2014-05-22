6df6386d0c18771e2762814f81850dbbbe9d2121
Sean Prasertsit <seanp.usa@gmail.com>
2014-05-22 15:43:35 -0700

    moved ontouch operations to touchview

--- app/src/main/java/edu/ucsb/cs/cs185/seanprasertsit/seanprasertsitmultitouch/app/MultiTouch.java
+++ app/src/main/java/edu/ucsb/cs/cs185/seanprasertsit/seanprasertsitmultitouch/app/MultiTouch.java
@@ -4,10 +4,8 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.PointF;
-import android.graphics.RectF;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -18,28 +16,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
 
 import java.io.File;
 
 
-public class MultiTouch extends ActionBarActivity implements View.OnTouchListener {
+public class MultiTouch extends ActionBarActivity {
 
-    // these matrices will be used to move and zoom image
-    private Matrix matrix = new Matrix();
-    private Matrix savedMatrix = new Matrix();
-    // we can be in one of these 3 states
-    private static final int NONE = 0;
-    private static final int DRAG = 1;
-    private static final int ZOOM = 2;
-    private int mode = NONE;
-    // remember some things for zooming
-    private PointF start = new PointF();
-    private PointF mid = new PointF();
-    private float oldDist = 1f;
-    private float d = 0f;
-    private float newRot = 0f;
-    private float[] lastEvent = null;
+
 
     private static int RESULT_LOAD_IMAGE = 1;
 
@@ -56,113 +39,10 @@ public class MultiTouch extends ActionBarActivity implements View.OnTouchListene
             Touchview myImage = (Touchview) findViewById(R.id.touchview);
             Bitmap bit = myImage.createBitmap(imgFile);
             myImage.setImageBitmap(bit);
-            myImage.setOnTouchListener(this);
         }
     }
 
-    public boolean onTouch(View v, MotionEvent event) {
-        // handle touch events here
-        Touchview view = (Touchview) v;
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
-                savedMatrix.set(matrix);
-                start.set(event.getX(), event.getY());
-                mode = DRAG;
-                lastEvent = null;
-                break;
-            case MotionEvent.ACTION_POINTER_DOWN:
-                oldDist = spacing(event);
-                if (oldDist > 10f) {
-                    savedMatrix.set(matrix);
-                    midPoint(mid, event);
-                    mode = ZOOM;
-                }
-                lastEvent = new float[4];
-                lastEvent[0] = event.getX(0);
-                lastEvent[1] = event.getX(1);
-                lastEvent[2] = event.getY(0);
-                lastEvent[3] = event.getY(1);
-                d = rotation(event);
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_POINTER_UP:
-                mode = NONE;
-                lastEvent = null;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (mode == DRAG) {
-                    matrix.set(savedMatrix);
-                    float dx = event.getX() - start.x;
-                    float dy = event.getY() - start.y;
-                    matrix.postTranslate(dx, dy);
-                } else if (mode == ZOOM) {
-                    float newDist = spacing(event);
-                    if (newDist > 10f) {
-                        matrix.set(savedMatrix);
-                        float scale = (newDist / oldDist);
-                        matrix.postScale(scale, scale, mid.x, mid.y);
-                    }
-                    if (lastEvent != null && event.getPointerCount() == 3) {
-                        newRot = rotation(event);
-                        float r = newRot - d;
-                        float[] values = new float[9];
-                        matrix.getValues(values);
-                        float tx = values[2];
-                        float ty = values[5];
-                        float sx = values[0];
-                        float xc = (view.getWidth() / 2) * sx;
-                        float yc = (view.getHeight() / 2) * sx;
-                        matrix.postRotate(r, tx + xc, ty + yc);
-                    }
-                }
-                break;
-        }
-
-        view.setImageMatrix(matrix);
-        return true;
-    }
-
-    /**
-     * Determine the space between the first two fingers
-     */
-    private float spacing(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return FloatMath.sqrt(x * x + y * y);
-    }
-
-    /**
-     * Calculate the mid point of the first two fingers
-     */
-    private void midPoint(PointF point, MotionEvent event) {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        point.set(x / 2, y / 2);
-    }
-
-    /**
-     * Calculate the degree to be rotated by.
-     *
-     * @param event
-     * @return Degrees
-     */
-    private float rotation(MotionEvent event) {
-        double delta_x = (event.getX(0) - event.getX(1));
-        double delta_y = (event.getY(0) - event.getY(1));
-        double radians = Math.atan2(delta_y, delta_x);
-        return (float) Math.toDegrees(radians);
-    }
-
-    /*public void filler() {
-        Touchview view = (Touchview)findViewById(R.id.touchview);
-        Matrix m = view.getImageMatrix();
-        RectF drawableRect = new RectF(0, 0, imageWidth, imageHeight);
-        RectF viewRect = new RectF(0, 0, view.getWidth(), view.getHeight());
-        m.setRectToRect(drawableRect, viewRect, Matrix.ScaleToFit.CENTER);
-        view.setImageMatrix(m);
-    }*/
-
-
+    // Inflate menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
@@ -171,7 +51,7 @@ public class MultiTouch extends ActionBarActivity implements View.OnTouchListene
         return true;
     }
 
-
+    // Action bar actions
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -194,11 +74,13 @@ public class MultiTouch extends ActionBarActivity implements View.OnTouchListene
         return super.onOptionsItemSelected(item);
     }
 
+    // Help popup
     public void showHelpDialog() {
         DialogFragment dialog = new HelpFrag();
         dialog.show(getFragmentManager(), "HelpDialog");
     }
 
+    // Settings popup
     public void showSettingsDialog() {
         DialogFragment dialog = new SettingsFrag();
         dialog.show(getFragmentManager(), "SettingsDialog");
--- app/src/main/java/edu/ucsb/cs/cs185/seanprasertsit/seanprasertsitmultitouch/app/Touchview.java
+++ app/src/main/java/edu/ucsb/cs/cs185/seanprasertsit/seanprasertsitmultitouch/app/Touchview.java
@@ -8,35 +8,53 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.PointF;
+import android.text.method.Touch;
 import android.util.AttributeSet;
+import android.util.FloatMath;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.io.File;
 
-/**
- * Created by SilverWolf on 5/20/14.
- */
 public class Touchview extends ImageView {
 
+    // these matrices will be used to move and zoom image
+    private Matrix matrix = new Matrix();
+    private Matrix savedMatrix = new Matrix();
+    // we can be in one of these 3 states
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+    // remember some things for zooming
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+    private float oldDist = 1f;
+    private float d = 0f;
+    private float newRot = 0f;
+    private float[] lastEvent = null;
+
+    //initiate variables
     Bitmap bitmap;
     Paint paint = new Paint();
     Point point = new Point();
 
+    // constructors
     public Touchview(Context context) {
         super(context);
     }
 
     public Touchview(Context context, AttributeSet attrs) {
         super(context, attrs);
-        // TODO Auto-generated constructor stub
     }
 
     public Touchview(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        // TODO Auto-generated constructor stub
     }
 
+    // onDraw function for the Touchview subclass
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -48,9 +66,106 @@ public class Touchview extends ImageView {
         paint.setStrokeWidth(15);
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawCircle(point.x, point.y, 20, paint);
+        this.setImageMatrix(matrix);
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // handle touch events here
+        Touchview view = (Touchview) findViewById(R.id.touchview);
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                lastEvent = null;
+                point.x = event.getX();
+                point.y = event.getY();
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                lastEvent = new float[4];
+                lastEvent[0] = event.getX(0);
+                lastEvent[1] = event.getX(1);
+                lastEvent[2] = event.getY(0);
+                lastEvent[3] = event.getY(1);
+                d = rotation(event);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    float dx = event.getX() - start.x;
+                    float dy = event.getY() - start.y;
+                    matrix.postTranslate(dx, dy);
+                } else if (mode == ZOOM) {
+                    float newDist = spacing(event);
+                    if (newDist > 10f) {
+                        matrix.set(savedMatrix);
+                        float scale = (newDist / oldDist);
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                    if (lastEvent != null && event.getPointerCount() == 2) {//was 3
+                        newRot = rotation(event);
+                        float r = newRot - d;
+                        float[] values = new float[9];
+                        matrix.getValues(values);
+                        float tx = values[2];
+                        float ty = values[5];
+                        float sx = values[0];
+                        float xc = (view.getWidth() / 2) * sx;
+                        float yc = (view.getHeight() / 2) * sx;
+                        matrix.postRotate(r, tx + xc, ty + yc);
+                    }
+                }
+                break;
+        }
+        invalidate();
+        //view.setImageMatrix(matrix);
+        return true;
+    }
+
+    /**
+     * Determine the space between the first two fingers
+     */
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return FloatMath.sqrt(x * x + y * y);
+    }
+
+    /**
+     * Calculate the mid point of the first two fingers
+     */
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
+    /**
+     * Calculate the degree to be rotated by.
+     *
+     * @param event
+     * @return Degrees
+     */
+    private float rotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
+    }
+
+    /*@Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
@@ -60,13 +175,15 @@ public class Touchview extends ImageView {
         }
         invalidate();
         return true;
-    }
+    }*/
 
+    // load bitmap
     public Bitmap createBitmap(File f) {
         bitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
         return bitmap;
     }
 
+    // Point class replacement
     class Point {
         float x, y;
     }
